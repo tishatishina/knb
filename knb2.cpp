ea3d479a7d9bc6782d035f4d5561095955a2c7b3
gameschool-student1 <stud1.gschl24@outlook.com>
2025-05-01 19:59:39 +0700

    тест

---
+++ knb2.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+
+void game() {
+
+		int a = 1;
+
+	for (int z = 1; z < 4;)
+	{
+	std::cout << "1.Камень 2.Ножницы 3.Бумага" << std::endl;
+		int bot = 1 + rand() % 3;
+		std::cin >> a;
+
+		//
+
+		if (a == 1 && bot == 2 || a == 2 && bot == 3 || a == 3 && bot == 1)
+		{
+			std::cout << "победа" << std::endl;
+			z++;
+		}
+		else if (a == bot)
+		{
+			std::cout << "ничья" << std::endl;
+		}
+		else
+		{
+			std::cout << "поражение" << std::endl;
+		}
+	}
+}
+
+
+
+
+int main() {
+		setlocale(LC_ALL, "Russian");
+		srand(time(NULL));
+		//game();
+		
+
+		/*char i[4] = { 't','e','s','t' };
+
+		for (int a = 0; a < 4; a++) {
+
+			std::cout << i[a];
+
+		}*/
+
+		/*std::string t = "test";
+		std::cout << t;
+		std::cin >> t;
+		std::cout << t;*/
+
+		
+}
+
