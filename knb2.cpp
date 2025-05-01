6e01037c856a6d9df39ab18a15685bd0599c9707
gameschool-student1 <stud1.gschl24@outlook.com>
2025-05-01 20:26:04 +0700

    русский язык, и потом исправить код

--- knb2.cpp
+++ knb2.cpp
@@ -1,32 +1,36 @@
 ﻿#include <iostream>
 
-void game() {
-
-		int a = 1;
-
-	for (int z = 1; z < 4;)
-	{
-	std::cout << "1.Камень 2.Ножницы 3.Бумага" << std::endl;
-		int bot = 1 + rand() % 3;
-		std::cin >> a;
-
-		//
-
-		if (a == 1 && bot == 2 || a == 2 && bot == 3 || a == 3 && bot == 1)
-		{
-			std::cout << "победа" << std::endl;
-			z++;
-		}
-		else if (a == bot)
-		{
-			std::cout << "ничья" << std::endl;
-		}
-		else
-		{
-			std::cout << "поражение" << std::endl;
-		}
-	}
-}
+//void game() {
+//
+//	std::string a;
+//
+//	std::string bot[3]{ "камень","ножницы","бумага" };
+//
+//	for (int z = 1; z < 4;)
+//	{
+//
+//	std::cout << "1.Камень 2.Ножницы 3.Бумага" << std::endl;
+//	int r = rand() % 3;
+//
+//		std::cin >> a;
+//
+//		
+//		// исправить !!!!!!!!!!!!!!!!!
+//		if (a == "камень" && bot[r] == "ножницы" || a == 2 && bot == 3 || a == 3 && bot == 1)
+//		{
+//			std::cout << "победа" << std::endl;
+//			z++;
+//		}
+//		else if (a == bot)
+//		{
+//			std::cout << "ничья" << std::endl;
+//		}
+//		else
+//		{
+//			std::cout << "поражение" << std::endl;
+//		}
+//	}
+//}
 
 
 
@@ -35,7 +39,7 @@ int main() {
 		setlocale(LC_ALL, "Russian");
 		srand(time(NULL));
 		//game();
-		
+		//сохранял русский язык в переменную стринг
 
 		/*char i[4] = { 't','e','s','t' };
 
@@ -45,10 +49,9 @@ int main() {
 
 		}*/
 
-		/*std::string t = "test";
-		std::cout << t;
+		std::string t = "test";
 		std::cin >> t;
-		std::cout << t;*/
+		std::cout << t;
 
 		
 }
