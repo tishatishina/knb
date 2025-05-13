10bbc2144b8856bdcbfd416af92f82d8ad56627d
Tisha <ilya_revenko@list.ru>
2025-05-13 15:06:21 +0700

    Всё исправил, всё работает

--- knb2.cpp
+++ knb2.cpp
@@ -1,58 +1,51 @@
 ﻿#include <iostream>
-
-//void game() {
-//
-//	std::string a;
-//
-//	std::string bot[3]{ "камень","ножницы","бумага" };
-//
-//	for (int z = 1; z < 4;)
-//	{
-//
-//	std::cout << "1.Камень 2.Ножницы 3.Бумага" << std::endl;
-//	int r = rand() % 3;
-//
-//		std::cin >> a;
-//
-//		
-//		// исправить !!!!!!!!!!!!!!!!!
-//		if (a == "камень" && bot[r] == "ножницы" || a == 2 && bot == 3 || a == 3 && bot == 1)
-//		{
-//			std::cout << "победа" << std::endl;
-//			z++;
-//		}
-//		else if (a == bot)
-//		{
-//			std::cout << "ничья" << std::endl;
-//		}
-//		else
-//		{
-//			std::cout << "поражение" << std::endl;
-//		}
-//	}
-//}
-
-
-
+#include <windows.h>
+#include <string>
 
 int main() {
-		setlocale(LC_ALL, "Russian");
-		srand(time(NULL));
-		//game();
-		//сохранял русский язык в переменную стринг
+void game();
+srand(time(NULL));
+std::setlocale(LC_ALL, "Russian");
 
-		/*char i[4] = { 't','e','s','t' };
-
-		for (int a = 0; a < 4; a++) {
-
-			std::cout << i[a];
-
-		}*/
-
-		std::string t = "test";
-		std::cin >> t;
-		std::cout << t;
+SetConsoleCP(1251);
+SetConsoleOutputCP(1251);
+	
+std::string a;
 
 		
+	std::string bot[3]{ "Камень","Ножницы","Бумага" };
+
+	
+	for (int z = 1; z < 4;)
+	{
+	
+		
+	std::cout << "1.Камень 2.Ножницы 3.Бумага" << std::endl;
+	
+		std::cin >> a;
+		
+		int r = rand() % 3;
+
+		std::cout  << bot[r] << std::endl;
+		
+		if (a == "Камень" && bot[r] == "Ножницы" || a == "Ножницы" && bot[r] == "Бумага" || a == "Бумага" && bot[r] == "Камень")
+		{
+			std::cout << "победа" << std::endl;
+			z++;
+		}
+		else if (a == bot[r])
+		{
+			std::cout << "ничья" << std::endl;
+		}
+		else
+		{
+			std::cout << "поражение" << std::endl;
+		}
+	}
+	
 }
+
+
+
+
 
